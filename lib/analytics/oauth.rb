c408ba85a12802c5be6609bec266833ce83e5379
Alexis Montagne <alexis.montagne@gmail.com>
2013-11-18 16:17:23 +0100

    Update with a better access_token for OAuth2

--- lib/analytics/oauth.rb
+++ lib/analytics/oauth.rb
@@ -4,22 +4,22 @@ require "oauth"
 module Analytics
   class OAuth
     class << self
-    def oauth_consumer
-      @consumer ||= ::OAuth::Consumer.new(Analytics.consumer_key, Analytics.consumer_secret, {
+      def oauth_consumer
+        @consumer ||= ::OAuth::Consumer.new(Analytics.consumer_key, Analytics.consumer_secret, {
           :site => 'https://www.google.com',
           :request_token_path => '/accounts/OAuthGetRequestToken',
           :access_token_path => '/accounts/OAuthGetAccessToken',
           :authorize_path => '/accounts/OAuthAuthorizeToken'
         })
-    end
-
-    def access_token(token, secret = nil, opts = {})
-      if token.is_a? ::OAuth::AccessToken
-        token
-      else
-        ::OAuth::AccessToken.new(oauth_consumer, token, secret)
       end
-    end
+
+      def access_token(token, secret = nil, opts = {})
+        if token.is_a? ::OAuth::AccessToken
+          token
+        else
+          ::OAuth::AccessToken.new(oauth_consumer, token, secret)
+        end
+      end
     end
   end
 end
